e18c4c093c1e39fe8401d47d678a692fbbef9791
Mohsin A. <hmdmohsinali@gmail.com>
2024-10-11 15:00:29 +0500

    big fixed

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const corsOptions = {
     origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
   };
   
 app.use(cors(corsOptions));
