5edd01c7edf8b18a6236525c68d2e9f053b0759b
Mohsin A. <hmdmohsinali@gmail.com>
2024-10-19 17:33:50 +0500

    done

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app= express();
 const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
-    origin: "*",
+    origin: ["*", "http://localhost:5173"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
