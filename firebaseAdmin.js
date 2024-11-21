4a19b5353a337387a96a7b36a1659e3b75cb1c26
hmdmohsinali <hmdmohsinali@gmail.com>
2024-11-21 14:40:57 +0500

    done with bugs

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,7 +1,9 @@
 import admin from "firebase-admin";
+import dotenv from "dotenv";
+dotenv.config();
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
+console.log(serviceAccount)
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
