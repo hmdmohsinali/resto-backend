b50ea8a644ac2f30d5f3b060874d604b65aa0f38
hmdmohsinali <hmdmohsinali@gmail.com>
2025-01-15 12:24:01 +0500

    check now

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -6,6 +6,7 @@ const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://resto-1dccd-default-rtdb.firebaseio.com"
 });
 
 export default admin;
