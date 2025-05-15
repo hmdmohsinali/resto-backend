b0dd517fe7763ef7c15a3e1912d56fc9874d2780
hmdmohsinali <hmdmohsinali@gmail.com>
2025-05-15 13:04:40 +0500

    cors addede

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app= express();
 const PORT = process.env.PORT || 4000
 const corsOptions = {
-    origin: ["*", "http://localhost:5173","http://localhost:5174", "https://resto-super-admin.vercel.app", "https://resto-sub-admins.vercel.app"],
+    origin: ["*", "http://localhost:5173","http://localhost:5174","https://resto-sub-admins-v464.vercel.app", "https://resto-super-admin.vercel.app", "https://resto-sub-admins.vercel.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
